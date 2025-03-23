6181800511fb8b4398018d8066a2be00c4bc7270
kaique-psantos <kaique212pereira@gmail.com>
2025-03-22 23:06:06 -0300

    Login adicionado

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import requests
 import google.generativeai as genai
 import time
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 from uuid import uuid4
 import os
 from dotenv import load_dotenv
+from firebase_admin import auth
+import requests
+import json
 
 # ------------------------------ CONFIGURAÇÃO INICIAL STREAMLIT -------------------------------
 st.set_page_config(
@@ -14,23 +17,20 @@ st.set_page_config(
     layout="centered",
     page_icon="https://img.icons8.com/?size=100&id=GjCK2f2wpZxt&format=png&color=000000"
 )
-#-----------------------------------------------------------------------------------------------
-
-
+# -----------------------------------------------------------------------------------------------
 
 # ------------------------------ CONFIGURAÇÃO DO GEMINI ----------------------------------------
-#Setando a Chave da API Gemini
 load_dotenv()
-key = os.getenv("API_KEY")
-genai.configure(api_key=key)
+key_gemini = os.getenv("API_KEY_GEMINI")
+key_firebase = os.getenv("API_KEY_FIREBASE")
+genai.configure(api_key=key_gemini)
 
-#Função para carregar MODELO
 @st.cache_resource
 def load_gemini():
     return genai.GenerativeModel('gemini-1.5-pro')
 
 model = load_gemini() 
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 
 # ------------------------------ CONFIGURAÇÃO FIREBASE --------------------------------------------
 if not firebase_admin._apps:
@@ -39,11 +39,53 @@ if not firebase_admin._apps:
 
 db = firestore.client()
 
+# ------------------------------ FUNÇÕES DE AUTENTICAÇÃO FIREBASE ------------------------------
+BASE_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={key_firebase}"
+# Função para autenticar o usuário (Login)
+def login(email, senha):
+    try:
+        payload = {
+            "email": email,
+            "password": senha,
+            "returnSecureToken": True
+        }
+        response = requests.post(BASE_URL, data=json.dumps(payload), headers={"Content-Type": "application/json"})
+        
+        if response.status_code == 200:
+            data = response.json()
+            return {"user_id": data["localId"], "id_token": data["idToken"]}
+        else:
+            print("Erro ao autenticar:", response.json().get("error", {}).get("message", "Desconhecido"))
+            return None
+    except Exception as e:
+        print(f"Erro ao autenticar: {str(e)}")
+        return None
+
+# Função para criar um novo usuário
+def create_account(email, senha):
+    try:
+        # Criação de usuário com e-mail e senha
+        user = auth.create_user(
+            email=email,
+            password=senha
+        )
+        return user.uid
+    except auth.AuthError as e:
+        print(f"Erro ao criar conta: {str(e)}")
+        return None
+
+# ------------------------------ LÓGICA PARA SELEÇÃO DO USUÁRIO ----------------------------
+
+# Função que retorna o ID do usuário atual
 def get_user_id():
-    return "global_chat"  # QUANDO FOR COLOCAR POR USUÁRIO ISSO AQUI TEM QUE MUDAR, TÁ GERAL
+    if "user_id" in st.session_state:
+        return st.session_state.user_id
+    return None
 
 def load_chat_history():
     user_id = get_user_id()
+    if user_id is None:
+        return []  # Nenhum usuário logado
     doc_ref = db.collection("chats").document(user_id)
     doc = doc_ref.get()
     if doc.exists:
@@ -52,6 +94,8 @@ def load_chat_history():
 
 def save_chat_message(role, message, image_url=None):
     user_id = get_user_id()
+    if user_id is None:
+        return  # Nenhum usuário logado, não salva
     doc_ref = db.collection("chats").document(user_id)
     
     new_message = {
@@ -65,17 +109,8 @@ def save_chat_message(role, message, image_url=None):
         "history": firestore.ArrayUnion([new_message]),
         "last_modified": firestore.SERVER_TIMESTAMP
     }, merge=True)
-#------------------------------------------------------------------------------------------------------
 
-# ------------------------------ INICIALIZAÇÕES --------------------------------------------------------
-if "conversation_history" not in st.session_state:
-    st.session_state.conversation_history = load_chat_history()
-#-------------------------------------------------------------------------------------------------------
-
-
-
-# ------------------------------ FUNÇÕES AUXILIARES -----------------------------------------------------
-# Configuração do Front-End do Chat
+# ------------------------------ FUNCIONALIDADES DO CHATBOT -----------------------------------
 def display_chat():
     for entry in st.session_state.conversation_history:
         role = entry["role"]
@@ -170,11 +205,7 @@ def consultando_nome_agente(agente_input):
         if agente in agente_input:
             return agente
     return None
-#--------------------------------------------------------------------------------------------------------------------------
 
-
-
-# ------------------------------ LÓGICA DE RESPOSTAS -------------------------------------------------
 def generate_response(user_input):
     try:
         start_time = time.time()
@@ -243,51 +274,90 @@ def generate_response(user_input):
         return {"text": f"Erro no sistema: {str(e)}", "image_url": None}
 
 
-# ------------------------------ INTERFACE -------------------------------------------------------------------------------------------------------------------
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image("https://img.icons8.com/?size=100&id=GjCK2f2wpZxt&format=png&color=000000", width=80)
-with col2:
-    st.title("Chatbot Riot Valorant")
-    st.caption("Faça perguntas sobre o melhor FPS existente!")
+# ------------------------------ INTERFACE DE LOGIN E CADASTRO ----------------------------
 
-with st.container(height=500, border=True):
-    display_chat()
+def login_or_register():
+    st.title("Login ou Cadastro")
+    
+    # Opções de Login ou Cadastro
+    login_or_register = st.radio("Escolha uma opção", ["Login", "Cadastro"])
+    
+    email = st.text_input("Digite seu e-mail")
+    senha = st.text_input("Digite sua senha", type="password")
+    
+    if login_or_register == "Login":
+        if st.button("Entrar"):
+            user_data = login(email, senha)
+            if user_data:
+                st.session_state.user_id = user_data["user_id"]
+                st.session_state.conversation_history = load_chat_history()
+                st.success("Login realizado com sucesso!")
+                st.experimental_rerun()
+            else:
+                st.error("Erro no login. Verifique suas credenciais.")
+    
+    elif login_or_register == "Cadastro":
+        if st.button("Cadastrar"):
+            user_id = create_account(email, senha)
+            if user_id:
 
-with st.form(key="chat_form", clear_on_submit=True):
-    user_input = st.text_input("Digite sua pergunta:", placeholder="Me fale sobre o Valorant.")
-    submit_button = st.form_submit_button("Enviar ➤")
+                st.success("Cadastro realizado com sucesso!")
 
-if submit_button and user_input:
-    with st.spinner("Buscando dados sobre Valorant..."):
-        # Salva mensagem do usuário
-        save_chat_message("user", user_input)
-        st.session_state.conversation_history.append({
-            "role": "user",
-            "message": user_input,
-            "timestamp": time.strftime("%H:%M:%S")
-        })
-        
-        resposta = generate_response(user_input)
-        
-        # Salva resposta do assistente
-        save_chat_message("assistant", resposta["text"], resposta["image_url"])
-        st.session_state.conversation_history.append({
-            "role": "assistant",
-            "message": resposta["text"],
-            "image_url": resposta["image_url"],
-            "timestamp": time.strftime("%H:%M:%S")
-        })
-        
-        st.rerun()
+            else:
+                st.error("Erro ao criar a conta.")
 
-st.markdown("---")
-st.markdown(
-    f"""
-    <div style='text-align: center; font-size: 0.9em; color: #666;'>
-        Desenvolvido por Gildemberg Lopes, Kaíque Pereira e Maria Fernanda • Versão Valorant • {time.strftime('%d/%m/%Y')} 
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-# -------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------ RENDERIZAÇÃO PRINCIPAL --------------------------------------
+
+if "user_id" not in st.session_state:
+    login_or_register()
+else:
+    
+    col1, col2 = st.columns([1, 4])
+    with col1:
+        st.image("https://img.icons8.com/?size=100&id=GjCK2f2wpZxt&format=png&color=000000", width=80)
+    with col2:
+        st.title("Chatbot Riot Valorant")
+        st.caption("Faça perguntas sobre o melhor FPS existente!")
+
+    with st.container(height=500, border=True):
+        display_chat()
+
+    with st.form(key="chat_form", clear_on_submit=True):
+        user_input = st.text_input("Digite sua pergunta:", placeholder="Me fale sobre o Valorant.")
+        submit_button = st.form_submit_button("Enviar ➤")
+
+    if submit_button and user_input:
+        with st.spinner("Buscando dados sobre Valorant..."):
+            save_chat_message("user", user_input)
+            st.session_state.conversation_history.append({
+                "role": "user",
+                "message": user_input,
+                "timestamp": time.strftime("%H:%M:%S")
+            })
+            
+            resposta = generate_response(user_input)
+            
+            save_chat_message("assistant", resposta["text"], resposta["image_url"])
+            st.session_state.conversation_history.append({
+                "role": "assistant",
+                "message": resposta["text"],
+                "image_url": resposta["image_url"],
+                "timestamp": time.strftime("%H:%M:%S")
+            })
+            
+            st.rerun()
+    if st.button("Logout"):
+        # Limpa o estado de sessão, efetivamente desconectando o usuário
+        del st.session_state.user_id
+        st.session_state.conversation_history = []  # Limpa o histórico de conversa
+        st.success("Logout realizado com sucesso!")
+        st.experimental_rerun()
+    st.markdown("---")
+    st.markdown(
+        f"""
+        <div style='text-align: center; font-size: 0.9em; color: #666;' >
+            Desenvolvido por Gildemberg Lopes, Kaíque Pereira e Maria Fernanda • Versão Valorant • {time.strftime('%d/%m/%Y')} 
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
